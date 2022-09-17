5720aa6a7034e600ce7f00c43e0be0db48a1c068
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-09-17 22:37:21 +0300

    Loggeradd logger

---
+++ TaskAppAPI/Application/Logger/LoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Logger
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string _path;
+        public FileLoggerProvider(string path)
+        {
+            _path = path;
+            File.WriteAllText(path, string.Empty);
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(_path);
+        }
+
+        public void Dispose() { }
+    }
+}
